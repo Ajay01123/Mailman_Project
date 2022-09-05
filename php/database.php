52389ff13444f0382c92f6662ba3d03d238afabc
Ajay01123 <ajayhestabit162@gmail.com>
2022-09-05 13:25:47 +0530

    change

--- php/database.php
+++ php/database.php
@@ -134,7 +134,7 @@ class Register
   }
   public function forget_password($password, $token)
   {
-    $update = "UPDATE `Register_tb` SET password= '$password', reset_date =NULL  WHERE reset_token= '$token'";
+    $update = "UPDATE Register_tb SET `password`= '$password', reset_date =NULL  WHERE reset_token= '$token'";
 
     if (mysqli_query($this->conn, $update)) {
       $_SESSION['user'] = "Password Updated Successfully";
